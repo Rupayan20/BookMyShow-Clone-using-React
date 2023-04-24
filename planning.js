26254155ceebebae3040a3b78eacb37f88106337
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-04-24 11:39:08 +0530

    Create planning.js

---
+++ planning.js
@@ -0,0 +1,20 @@
+// bookmyshow planning
+
+// --> We need a reusable carousel
+  // --> Hero carousel
+  // --> Common carousel
+  // --> Cast carousel (small carousel)
+
+// Pages
+  // --> Home Page
+  // --> Movie Page
+  // --> Categories Page
+  // --> Auth Page
+  // --> Razorpay payment gateway
+
+// Plan how we are building it?
+
+// Layouts
+// Routes
+// Pages
+// Components
